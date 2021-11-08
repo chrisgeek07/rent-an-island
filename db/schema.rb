dffb8fe6e75607df1f10aebc1535869e4fbb05d5
Christian BeauxYeux <gildweek@gmail.com>
2021-11-08 15:29:42 +0400

    Created DB & migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_11_08_104714) do
+ActiveRecord::Schema.define(version: 2021_11_08_112124) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "islands", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.integer "price"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_islands_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "name"
@@ -22,4 +32,5 @@ ActiveRecord::Schema.define(version: 2021_11_08_104714) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  add_foreign_key "islands", "users"
 end
