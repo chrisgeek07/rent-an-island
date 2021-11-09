95c839e4607410aa1f932d528b3c96f6b5676bdb
Christian BeauxYeux <gildweek@gmail.com>
2021-11-09 15:14:32 +0400

    Added commas where needed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,26 +8,26 @@
 puts "Creating New islands..."
 
 Island.create(
-  name: "Kauai"
-  description: "Kauai is an island in the Central Pacific, part of the Hawaiian archipelago. It's nicknamed 'the Garden Isle' thanks to the tropical rainforest covering much of its surface."
+  name: "Kauai",
+  description: "Kauai is an island in the Central Pacific, part of the Hawaiian archipelago. It's nicknamed 'the Garden Isle' thanks to the tropical rainforest covering much of its surface.",
   price: 1099
 )
 
 Island.create(
-    name: "Tasmania"
-    description: "Tasmania, abbreviated as TAS, is an island state of Australia. It is located 240 km to the south of the Australian mainland, separated from it by Bass Strait."
+    name: "Tasmania",
+    description: "Tasmania, abbreviated as TAS, is an island state of Australia. It is located 240 km to the south of the Australian mainland, separated from it by Bass Strait.",
     price: 899
 )
 
 Island.create(
-    name: "Lifou"
-    description: "Lifou Island, historically spelt Lifu or Lefu in English, and known as Drehu in the local language, is the largest, most populous and most important island of the Loyalty Islands, in the archipelago of New Caledonia, an overseas territory of France in the Pacific Ocean."
+    name: "Lifou",
+    description: "Lifou Island, historically spelt Lifu or Lefu in English, and known as Drehu in the local language, is the largest, most populous and most important island of the Loyalty Islands, in the archipelago of New Caledonia, an overseas territory of France in the Pacific Ocean.",
     price: 1099
 )
 
 Island.create(
-    name: "Tahiti"
-    description: "Tahiti is the largest island in French Polynesia, the South Pacific archipelago. Shaped like a figure-8, it's divided into Tahiti Nui (the larger, western section) and Tahiti Iti (the eastern peninsula)."
+    name: "Tahiti",
+    description: "Tahiti is the largest island in French Polynesia, the South Pacific archipelago. Shaped like a figure-8, it's divided into Tahiti Nui (the larger, western section) and Tahiti Iti (the eastern peninsula).",
     price: 1499
 )
 
