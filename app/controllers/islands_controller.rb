276114900b011d17d6e6c9da9a520802919eaccc
Christian BeauxYeux <gildweek@gmail.com>
2021-11-09 12:29:27 +0400

    created island views and controller with basic styling

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -3,6 +3,43 @@ class IslandsController < ApplicationController
     @islands = Island.all
   end
 
+  def new
+    @island = Island.new
+  end
+
+  def create
+    @island = Island.new(island_params)
+    if @island.save
+      redirect_to islands_path(@island)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @island = Island.find(params[:id])
+  end
+
+  def edit
+    @island = Island.find(params[:id])
+  end
+
+  def update
+    @island = Island.find(params[:id])
+    @island.update(island_params)
+    redirect_to islands_path
+  end
+
+  def destroy
+    @island = Island.find(params[:id])
+    @island.destroy
+    redirect_to islands_path
+  end
+
   private
-  
+
+  def island_params
+    params.require(:island).permit(:name, :description, :price)
+  end
+
 end
