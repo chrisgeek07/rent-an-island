63cf55b17c34a084f68ae91cca9906e91fd353db
vilorshini ramsamy <vilorshiniramsamy02@gmail.com>
2021-11-09 17:04:23 +0400

    linked all booking pages

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,21 +10,27 @@ class BookingsController < ApplicationController
   end
 
   def create
-    @booking = Booking.new(booking_params)
+    @booking = Booking.create(booking_params)
     @booking.island = @island
+    @booking.user = current_user
     if @booking.save
-      redirect_to island_booking_path
+      redirect_to island_booking_path(@island, @booking)
     else
       render :new
     end
   end
 
   def show
+    @booking = Booking.find(params[:id])
   end
 
   private
 
   def set_booking
     @island = Island.find(params[:island_id])
+  end
+
+  def booking_params
+    params.require(:booking).permit(:island_id, :start_date, :end_date)
   end
 end
