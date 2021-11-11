3326c82d4b0e538b39b401075c2037d96e6bdd7d
Christian BeauxYeux <gildweek@gmail.com>
2021-11-11 14:58:53 +0400

    Added geocodes in island model

--- app/models/island.rb
+++ app/models/island.rb
@@ -1,4 +1,6 @@
 class Island < ApplicationRecord
+  geocoded_by :name
+  after_validation :geocode, if: :will_save_change_to_name?
   belongs_to :user
   has_many :bookings, dependent: :destroy
   has_one_attached :photo
