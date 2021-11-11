7baf3e326bc14186d0fe22c26b8cad22d752eb92
Christian BeauxYeux <gildweek@gmail.com>
2021-11-11 17:18:05 +0400

    Added markers pop-up with some basic styling

--- app/controllers/islands_controller.rb
+++ app/controllers/islands_controller.rb
@@ -4,7 +4,8 @@ class IslandsController < ApplicationController
     @markers = @islands.geocoded.map do |island|
       {
         lat: island.latitude,
-        lng: island.longitude
+        lng: island.longitude,
+        info_window: render_to_string(partial: "info_window", locals: { island: island })
       }
     end
     if params[:query].present?
